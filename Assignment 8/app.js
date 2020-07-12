c998ece71361f391f01e801d914bf07e56d259f0
Barira <barirakhan1@gmail.com>
2020-07-12 20:46:02 +0500

    modification in ass8 done

--- Assignment 8/app.js
+++ Assignment 8/app.js
@@ -19,18 +19,24 @@ function timer(){
         minHeading.innerHTML = min;
     }
 }
+var e= document.getElementById("start")
+var d= document.getElementById("stop")
+
 function start(){
     interval = setInterval(timer,10)
+    e.disabled=true
 }
 function stop(){
     clearInterval(interval);
+    e.disabled=false
 }
 function reset(){
     min=0;
     sec=0;
     msec=0;
-    minHeading.innerHTML = min
-    secHeading.innerHTML = sec
-    msecHeading.innerHTML =msec
+    minHeading.innerHTML = 0
+    secHeading.innerHTML = 0
+    msecHeading.innerHTML = 0
     stop()
+    e.disabled=false
 }
